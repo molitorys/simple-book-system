3dd490d708df204d74aca92d8787c39fc4118e46
Stanislaw Molitorys <s.molitorys@gmail.com>
2019-01-24 11:11:33 +0100

    Poprawa widoku dla błędu 503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,16 +2,9 @@
 
 @section('content')
 
-<div class="container">
-        <article class="page_area not_found_page">
-            <h1 class="page_title centered">503</h1>
-            <div class="page_desc">
-                <p>Trwają prace deweloperskie.</p>
-                <p>Serwis został chwilowo wyłączony.</p>
-                <br>
-                <p>Wróć do nas za kilka chwil...</p>
-            </div>
-        </article>
+<div class="error-content">
+    <h2>Trwają prace deweloperskie.</h2>
+    <p>Serwis został chwilowo wyłączony.</p>
 </div>
 
 @endsection
